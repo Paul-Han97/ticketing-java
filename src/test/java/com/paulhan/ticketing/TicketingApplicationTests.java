6bced3f10b418b3019eb4ca1a0a5d64778124329
한바울 <wtg.paulhan@gmail.com>
2026-05-20 14:34:21 +0900

    Chore: 프로젝트 초기화

---
+++ src/test/java/com/paulhan/ticketing/TicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulhan.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
